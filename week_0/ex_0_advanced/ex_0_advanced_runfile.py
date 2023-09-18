780c8c07a18b760e09af57f4757fddcacd4787d0
Ilias <ilias.simon@outlook.fr>
2023-09-18 11:20:03 +0200

    assignment 0

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,44 @@
-#YOUR CODE FOR EX_0 ADVANCED HERE
+while True:
+    sequence = str(input("enter your DNA sequence: "))
+    sequence = sequence.upper()
+    valid = True
+    for i in sequence:
+        if i not in "ATGC":
+            print("please enter a valid DNA sequence (ATGC)")
+            valid = False
+            break
+    if valid:
+        break
+sequence_lenght = int(len(sequence))
+nA = 0
+nT = 0
+nG = 0
+nC = 0
+for i in sequence:
+    if i == "A":
+        nA +=1
+    else:
+        pass
+    if i == "T":
+        nT +=1
+    else:
+        pass
+    if i == "G":
+        nG +=1
+    else:
+        pass
+    if i == "C":
+        nC +=1
+GC_content = nG + nC
+GC = ((GC_content)/sequence_lenght)
+print("sequence lenght =" +str(sequence_lenght))
+print("number of A: " +str(nA))
+print("number of T: " +str(nT))
+print("number of G: " +str(nG))
+print("number of C: " +str(nC))
+print("number of GC= " +str(GC_content), end="")
+print(" with a proportion of" + str(GC))
+if GC > 0.5:
+    print(" Your sequence is rich in GC")
+else:
+    print(" Your sequence is poor in GC")
--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,7 @@
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+print("this program will calculate the Microbial Growth Rate based on your parameters, follow the instructions.")
+start= int(input("Enter the number of cells at the beginning: "))
+end= int(input("Enter the number of cells at the end: "))
+time= int(input("enter the time between start and end: "))
+import math
+Growth_rate= (math.log(end)-math.log(start))/time
+print("This population of cells have a GR of " + str(Growth_rate))
