24e18320c0f8e6362d7d57b0b5b0ecbd4d9aa536
LeoPecout <leoadrien.pecout@gmail.com>
2023-09-20 22:25:48 +0200

    ex_0_intermediate done!

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,15 @@
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+def calculate_growth_rate():
+
+        initial_count = int(input("Enter the initial cell count: "))
+        final_count = int(input("Enter the final cell count: "))
+        time = int(input("Enter the time elapsed: "))
+        if int(time) < 0:
+                return print('Only positive numbers for the time' )
+
+        growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+
+        print(f"The growth rate is: {growth_rate}")
+
+calculate_growth_rate()
+
