d83274b8daa299d1a63c6d34d39be2818707b25d
Anna DIAZ <anna.diaz@univ-tlse3.fr>
2023-09-20 20:38:36 +0200

    intermediate

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,14 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+
+import math
+
+initial_count = float(input("Type the initial cell count: "))
+final_count = float(input("Type the final cell count: "))
+time = float(input("Type the time: "))
+
+def GrowthRate(initial_count, final_count, time):
+    growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+    return growth_rate
+
+result = GrowthRate(initial_count, final_count, time)
+print("Growth Rate:", result)
