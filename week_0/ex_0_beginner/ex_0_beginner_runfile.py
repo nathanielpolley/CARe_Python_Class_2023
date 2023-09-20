e6655a993390ccc54a3486a63503d2d69f9b73a3
sophiebrgu <sb.bruguiere@gmail.com>
2023-09-20 18:57:03 +0200

    2 modifications

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -2,4 +2,4 @@ import this
 
 print("\n"+"hello, I am Sophie, a student in the CARe program. I would like to learn a programming language to acquire as many science skills as possible and also because I am a very curious person")
 
-print("\n"+"I think the last time that I was without computer available is when I was hiking in the mountains with my family")
+print("\n"+"I think the last time that I was without computer available is when I was hiking in the mountains with my family during holidays")
