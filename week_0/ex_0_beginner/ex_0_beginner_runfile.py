c41f15b76fafe238c43d8eecca1581073f977814
dbozoglou <dbozoglou@outlook.com>
2023-09-20 21:48:57 +0200

    Responses for Prompt 1 and 2.

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"I'm Dimitra, a second year Mater's student who loves traveling and dreaming! I study in France and my goal is to work on cancer research. Learning a programming language will help me to better understand how to analyze my results and how to work faster and more efficiently.")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"The last time I was without a computer available was during this summer on an island in Greece! Of course this happened just because my phone was dead and I left it charging, but the result is what matters! I spent some hours only with my book and beers :) ")
