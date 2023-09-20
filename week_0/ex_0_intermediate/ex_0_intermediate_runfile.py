39aec3716feba7978dcd2af75b260547b901201b
Ilias <ilias.simon@outlook.fr>
2023-09-20 22:03:33 +0200

    assignment 0

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,7 +1,16 @@
 print("this program will calculate the Microbial Growth Rate based on your parameters, follow the instructions.")
-start= int(input("Enter the number of cells at the beginning: "))
-end= int(input("Enter the number of cells at the end: "))
-time= int(input("enter the time between start and end: "))
+while True :
+    start= str(input("Enter the number of cells at the beginning: "))
+    end= str(input("Enter the number of cells at the end: "))
+    time= str(input("enter the time between start and end: "))
+    valid = True
+    for i in start:
+        if i not in "0123456789.":
+            valid = False
+            print("insert a valid number")
+            break
+    if valid:
+        break
 import math
-Growth_rate= (math.log(end)-math.log(start))/time
+Growth_rate= (math.log(int(end))-math.log(int(start)))/int(time)
 print("This population of cells have a GR of " + str(Growth_rate))
