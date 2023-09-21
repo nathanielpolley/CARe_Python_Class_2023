551369bd14fae47f4181c87acb146ad62ae8ac00
maureen <maureen.verdier@univ-tlse3.fr>
2023-09-21 14:57:51 +0200

    changes for exo_0 .py .txt

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+" I am a Maureen, student in second year of the CARe master's program and I aspire to become a researcher in oncology. In this field, programming is essential. Indeed, i'm already facing the urge to learn how to code since biology uses a lot of different software to treat the data.  ")
+print("\n"+" I am a Maureen, a student in second year of the CARe master's program and I aspire to become a researcher in oncology. In this field, programming is essential. Indeed, i'm already facing the urge to learn how to code since biology uses a lot of different software to treat the data.  ")
 
 print("\n"+" NEVER, always with my phone.")
