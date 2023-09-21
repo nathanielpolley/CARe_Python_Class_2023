a1cebcf3bbc1cc4b85a028e0d99bb1b8bb9f1739
hugoschodet <Hugo.Schodet@gmail.com>
2023-09-21 17:17:50 +0200

    Changing beginner runfile and submit text

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"PROMPT 1")
+print("\n"+"Hello i'm Hugo, i would like to be a be a cancer researcher  ")
 
-print("\n"+"PROMPT 2")
+print("\n"+"I think it was during camping")
