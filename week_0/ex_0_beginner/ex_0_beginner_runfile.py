3b1daac9cf9eddb913e405a6c4c6a0ee097dbb27
sophiebrgu <sb.bruguiere@gmail.com>
2023-09-21 18:42:16 +0200

    Pushed 3 commits to origin/master

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"hello, I am Sophie, a student in the CARe program. I would like to learn a programming language to acquire as many science skills as possible and also because I am a very curious person")
+print("\n"+"hello, I a  m Sophie, a student in the CARe program. I would like to learn a programming language to acquire as many science skills as possible and also because I am a very curious person")
 
 print("\n"+"I think the last time that I was without computer available is when I was hiking in the mountains with my family during holidays")
