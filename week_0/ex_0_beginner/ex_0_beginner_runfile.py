5ce7566437a70c380eadfa3b03e9811f5699394c
JeanineSubramoney <jeaninesubramoney@gmail.com>
2023-09-21 16:45:53 +0200

    Commit

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"My name is Jeanine Subramoney, a South African national enrolled in the CARe programme. I want to develop a new set of skills to aid my research goals.")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"Camping in 2016, worst June of my life!")
