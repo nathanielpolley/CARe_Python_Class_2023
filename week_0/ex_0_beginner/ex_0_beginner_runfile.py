f1bf2b9e6aa398d3a44f5797212b3cb22c4d3985
Jimmy150 <145379394+Jimmy150@users.noreply.github.com>
2023-09-21 17:34:44 +0200

    Update ex_0_beginner_runfile.py

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,5 +1,5 @@
 import this
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 1 HERE")
+print("\n"+"I am a determined student with a strong passion for cancer signaling and immunology. I am currently seeking opportunities for a Master's 2 thesis project that allows me to collaborate with fellow researchers and I aim learning programming in order to analye data result from RNA seq and other staristical test analysis")
 
-print("\n"+"TYPE YOUR RESPONSE TO PROMPT 2 HERE")
+print("\n"+"Camping in Alpes mountains in Grenoble in Dec 2022")
