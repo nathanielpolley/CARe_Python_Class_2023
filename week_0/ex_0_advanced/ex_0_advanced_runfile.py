cac3e23ecd5bb9c324c1f7ff1fb655fb11974423
MateoDufra <mateo.dufraisse@gmail.com>
2023-09-27 17:37:31 +0200

    Here my work

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,10 @@
 #YOUR CODE FOR EX_0 ADVANCED HERE
+Floup = input("Put your DNA sequence here : \n")
+DNA= Floup.upper()
+LDNA = len(DNA)
+A = DNA.count("A")
+G = DNA.count("G")
+C = DNA.count("C")
+T = DNA.count("T")
+GC = ((G+C)/LDNA)*100
+print("The length of the DNA sequence is %d long.\nThere is %d Adenine, %d Guanine, %d Cytosine and %d Thymine.\nThe GC content is about %.2f" %(LDNA,A,G,C,T,GC))
