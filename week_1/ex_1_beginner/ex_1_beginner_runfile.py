61672c9772d4b6eae8be2bc530aa2ff6a72b64d3
Ilias <ilias.simon@outlook.fr>
2023-09-30 19:16:30 +0200

    assignment 0

--- week_1/ex_1_beginner/ex_1_beginner_runfile.py
+++ week_1/ex_1_beginner/ex_1_beginner_runfile.py
@@ -1,1 +1,9 @@
 #YOUR CODE FOR EX_1 BEGINNER HERE
+lst= [100,200,150,300,50]
+average = 0
+for i in lst:
+    average += i
+average = average/5
+min = min(lst)
+max = max(lst)
+print("average= "+ str(average) +" Min= "+ str(min)+ " Max= "+ str(max))
