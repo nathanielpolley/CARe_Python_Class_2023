202c5338c1f413b2f73b4fa69ab67c971bf3174b
halashalhoub <halashalhoob@hotmail.com>
2023-10-01 18:34:39 +0300

    Level intermediate done Hala

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,10 +1,13 @@
 #DE FOR EX_0 INTERMEDIATE HERE
 import math
-initialcount = (input("Enter the  initialcount: "))
-finalcount = (input("Enter the finalcount: "))
-time = (input("Enter your time: "))
-if  finalcount > initialcount and initialcount.isdigit() and finalcount.isdigit() and time.isdigit():
-    GrowthRate = math.log(int(finalcount)) - math.log(int(initialcount)) / int(time)
-    print (str(GrowthRate))
+initialcount = input("Enter the  initialcount: ")
+finalcount = input("Enter the finalcount: ")
+time = input("Enter your time: ")
+if  initialcount.isdigit() and finalcount.isdigit() and time.isdigit():
+    if(int(finalcount) > int(initialcount)):
+        GrowthRate = math.log(int(finalcount)) - math.log(int(initialcount)) / int(time)
+        print (str(GrowthRate))
+    else:
+        print("invalidinput")
 else:
     print("invalidinput")
