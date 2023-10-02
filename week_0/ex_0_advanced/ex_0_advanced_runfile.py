215157ecd71864389c2b62a93ded02e4b184b897
LeoPecout <leoadrien.pecout@gmail.com>
2023-10-02 15:36:30 +0200

    Modified intermediate and advanced week 0 work according to your instructions.

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -2,12 +2,11 @@ dna_sequence = str(input('Add the DNA sequence here: '))
 dna_sequence = dna_sequence.upper()
 if all(nucleotides in "ACTG" for nucleotides in dna_sequence):
     print("DNA sequence valid.")
+    print(len(dna_sequence))
+    print("Number of Adenosine: ", dna_sequence.count("A"))
+    print("Number of Cytosine: ", dna_sequence.count("C"))
+    print("Number of Guanine: ", dna_sequence.count("G"))
+    print("Number of Thymine: ", dna_sequence.count("T"))
+    print("Number of GC: ", dna_sequence.count("GC") / len(dna_sequence))
 else:
     print("Give me only nucleotides in a DNA structure!")
-
-print(len(dna_sequence))
-print("Number of Adenosine: ", dna_sequence.count("A"))
-print("Number of Cytosine: ", dna_sequence.count("C"))
-print("Number of Guanine: ", dna_sequence.count("G"))
-print("Number of Thymine: ", dna_sequence.count("T"))
-print("Number of GC: ", dna_sequence.count("GC"))
--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,15 +1,17 @@
 import math
 def calculate_growth_rate():
-
+    try:
         initial_count = int(input("Enter the initial cell count: "))
         final_count = int(input("Enter the final cell count: "))
         time = int(input("Enter the time elapsed: "))
-        if int(time) < 0:
-                return print('Only positive numbers for the time' )
 
-        growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+        if time < 0:
+            print("Only positive numbers for the time are allowed.")
+        else:
+            growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+            print(f"The growth rate is: {growth_rate}")
+    except ValueError:
+        print("Invalid input. You need a number of cells that is greater than or equal to 0.")
 
-        print(f"The growth rate is: {growth_rate}")
 
 calculate_growth_rate()
-
