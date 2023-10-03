5d1d9a2e41077c5dcf70d4a36793843323449524
MatiasMedinaTanco <matias.mtanco@gmail.com>
2023-10-03 09:57:46 -0600

    Matias Medina Tanco - Made a second version of the ex_0_intermediate which takes into account all the possible incorrect inputs.

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -7,35 +7,29 @@
 # import the relevant library
 import math
 
-# Ask for initial cell count
+# start loop to repeatedly ask for inputs until they are all correct
 while True:
-    try:
-        initial = int(input("Enter the initial cell count: "))
-    except ValueError:
-        print("Please enter a valid, whole number.")
+    # Ask for the initial and final cell counts, as well as time elapsed
+    initial = input("Enter the initial cell count: ")
+    final = input("Enter the final cell count: ")
+    time = input("Enter the time elapsed: ")
+
+    # Check if the inputs are valid. If they are not valid then program will ask again for the inputs
+    if (final > initial) and initial.isdigit() and final.isdigit() and time.isdigit():
+        # Calculates growth rate and prints out result
+        initial = int(initial)
+        final = int(final)
+        time = int(time)
+
+        # Check if time is greater than 0 to prevent an error in the formula (the .isdigit() method
+        # allows zeroes to pass through)
+        if time > 0:
+            GrowthRate = (math.log(final) - math.log(initial)) / time
+            print("The growth rate of the microbial culture is: ", GrowthRate)
+            # Get out of loop to end program
+            break
+        else:
+            print("Please enter valid inputs.")
+
     else:
-        break
-
-# Ask for final cell count
-while True:
-    try:
-        final = int(input("Enter the final cell count: "))
-    except ValueError:
-        print("Please enter a valid, whole number.")
-    else:
-        break
-
-# Ask for time elapsed
-while True:
-    try:
-        time = int(input("Enter the time elapsed: "))
-    except ValueError:
-        print("Please enter a valid, whole number.")
-    else:
-        break
-
-# Calculates growth rate and prints out result
-GrowthRate = (math.log(final) - math.log(initial)) / time
-
-print("The growth rate of the microbial culture is: ", GrowthRate)
-
+        print("Please enter valid inputs.")
