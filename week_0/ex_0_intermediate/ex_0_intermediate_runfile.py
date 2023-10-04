eef08d96365a9e41e7445e2cf3a429c837d31d7b
pankajgarg <pankajgarg471@gmail.com>
2023-10-04 09:43:20 +0200

    Microbial Growth Rate Calculator

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,9 @@
+x= int(input("Initial Number of Cells"))
+y= int(input("Final Number of Cells"))
+z= int(input("Time Taken"))
+if (y>x): print ("Running Formula")
+else:
+    print ("The Final count must be greater")
+a= (y-x)/z
+print ("Microbial Growth Rate", a)
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
