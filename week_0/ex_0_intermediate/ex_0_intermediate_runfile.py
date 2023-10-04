344e5a34657c776b802c742b3618554d258bd137
pankajgarg <pankajgarg471@gmail.com>
2023-10-04 12:00:22 +0200

    Microbial Growth Rate Calculator

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,9 +1,19 @@
-x= int(input("Initial Number of Cells"))
-y= int(input("Final Number of Cells"))
-z= int(input("Time Taken"))
-if (y>x): print ("Running Formula")
+import math
+
+x = input("Initial Number of Cells: ")
+y = input("Final Number of Cells: ")
+z = input("Time Taken: ")
+
+if x.isdigit() and y.isdigit() and z.isdigit():
+    initial = int(x)
+    final = int(y)
+    time = int(z)
+
+    if y > x:
+        print("Running Formula")
+        growth_rate = (math.log(final) - math.log(initial)) / time
+        print("Microbial Growth Rate:", growth_rate)
+    else:
+        print("Error: The Final count must be greater than the Initial count.")
 else:
-    print ("The Final count must be greater")
-a= (y-x)/z
-print ("Microbial Growth Rate", a)
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+    print("Error: Please enter non-negative and non-decimal numeric values.")
