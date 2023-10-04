e902db94462a6f2be6b9e99126c1afb2659465ae
JulienPy <julien.letoile@uniiv-tlse3.fr>
2023-10-04 20:44:22 +0200

    ex_0_intermediate

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,18 @@
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+
+initial_count = input("Enter the initial cell count: ")
+final_count = input("Enter the final cell count: ")
+time = input("Enter the time elapsed: ")
+
+try:
+    initial_count = int(initial_count)
+    final_count = int(final_count)
+    time = float(time)
+    if initial_count <= 0 or final_count <= 0 or time <= 0:
+        print("You must enter a positive number")
+
+    else:
+        growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+        print(growth_rate)
+except ValueError:
+    print("Invalid input, please enter integer values for initial_count and final_count")
