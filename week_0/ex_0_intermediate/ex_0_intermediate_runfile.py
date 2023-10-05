cf85d2ffd7eedc84c5e1df8838989afc1ad6382e
Anna DIAZ <anna.diaz@univ-tlse3.fr>
2023-10-05 22:42:59 +0200

    advanced week0

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -2,13 +2,20 @@
 
 import math
 
-initial_count = float(input("Type the initial cell count: "))
-final_count = float(input("Type the final cell count: "))
-time = float(input("Type the time: "))
+initial_count = int(input("Type the initial cell count: "))
+final_count = int(input("Type the final cell count: "))
+time = int(input("Type the time: "))
+
 
 def GrowthRate(initial_count, final_count, time):
     growth_rate = (math.log(final_count) - math.log(initial_count)) / time
     return growth_rate
+if (final_count > initial_count>0) and (time>0) and (isinstance(initial_count,int)) and (isinstance(final_count,int)) and (isinstance(time,int)):
+    result = GrowthRate(initial_count, final_count, time)
+    print("Growth Rate:", result)
+else:
+    print("error ! remember : - your final count need to be higher than the initial count"
+          "        - time and initial count have to be positive"
+          "        - you need to enter a int")
 
-result = GrowthRate(initial_count, final_count, time)
-print("Growth Rate:", result)
+
