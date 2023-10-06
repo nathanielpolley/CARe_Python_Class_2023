c8ac467f9c39db96a19efd03110f84bb05a80d23
JeanineSubramoney <145459654+JeanineSubramoney@users.noreply.github.com>
2023-10-06 17:13:10 +0200

    Update ex_1_intermediate_runfile.py

--- week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
+++ week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
@@ -1,1 +1,22 @@
 #YOUR CODE FOR EX_1 INTERMEDIATE HERE
+#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+
+# User input
+import math
+
+x = input("Enter the initial number of cells as an integer only.")
+y = input("Enter the final number of cells as an integer only")
+t = input("Enter the amount of time in minutes")
+
+
+#User error correction
+if x.isdigit() and y.isdigit() and t.isdigit():
+    print("Thank you")
+
+ # microbial growth rate calculator
+    a = math.log(int(y)) - math.log(int(x)) / int(t)
+    a = round(a, 2)
+    print("Microbial growth is ", a)
+
+else:
+    print("Please enter only integer values")
