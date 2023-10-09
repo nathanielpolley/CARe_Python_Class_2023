61dcac3d68356b96625d1d89e5e7681b24a040c6
halashalhoub <halashalhoob@hotmail.com>
2023-10-10 00:37:03 +0300

    Merge remote-tracking branch 'origin/master'

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -2,18 +2,20 @@
 DNASeq = input("Enter the  DNA Sequence: ")
 DNASeq = DNASeq.upper()
 allowedLetters = ['A', 'C', 'G', 'T']
-DNAlength = len("DNASeq")
-Adenin_count = DNASeq.count('A')
-Cytosine_count = DNASeq.count('C')
-Guanine_count = DNASeq.count('G')
-Thymine_count = DNASeq.count('T')
-GC_content = DNASeq.count('GC')
 if any(words in DNASeq for words in allowedLetters):
+    DNAlength = len(DNASeq)
+    Adenine_count = DNASeq.count('A')
+    Cytosine_count = DNASeq.count('C')
+    Guanine_count = DNASeq.count('G')
+    Thymine_count = DNASeq.count('T')
+    addition = Cytosine_count + Guanine_count
+    Division = addition / DNAlength
+    percentage = Division * 100
     print(f"The DNA sequence length: {DNAlength}")
-    print(f"The number of adenine bases: {Adenin_count}")
+    print(f"The number of adenine bases: {Adenine_count}")
     print(f"The number of cytosine bases: {Cytosine_count}")
     print(f"The number of guanine bases: {Guanine_count}")
     print(f"The number of thymine bases: {Thymine_count}")
-    print(f"The GC content: {GC_content}")
+    print(f"The GC content: {percentage}")
 else:
      print("the DNA sequence is invalid")
