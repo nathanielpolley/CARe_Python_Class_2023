d87e48ec85d6e94ed616f2ef5e3dcd6ae7a52506
halashalhoub <halashalhoob@hotmail.com>
2023-10-09 21:07:31 +0300

    Level Advanced done Hala

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,19 @@
-#YOUR CODE FOR EX_0 ADVANCED HERE
+# import math
+DNASeq = input("Enter the  DNA Sequence: ")
+DNASeq = DNASeq.upper()
+allowedLetters = ['A', 'C', 'G', 'T']
+DNAlength = len("DNASeq")
+Adenin_count = DNASeq.count('A')
+Cytosine_count = DNASeq.count('C')
+Guanine_count = DNASeq.count('G')
+Thymine_count = DNASeq.count('T')
+GC_content = DNASeq.count('GC')
+if any(words in DNASeq for words in allowedLetters):
+    print(f"The DNA sequence length: {DNAlength}")
+    print(f"The number of adenine bases: {Adenin_count}")
+    print(f"The number of cytosine bases: {Cytosine_count}")
+    print(f"The number of guanine bases: {Guanine_count}")
+    print(f"The number of thymine bases: {Thymine_count}")
+    print(f"The GC content: {GC_content}")
+else:
+     print("the DNA sequence is invalid")
