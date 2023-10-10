29645f09fad9d4f77c693e3173c4d898890d3e1d
IsabellaOdaSilva <isabelladasilva085@gmail.com>
2023-10-10 23:08:59 +0200

    Code for microbial growth calculator

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -6,11 +6,11 @@ initial_count = input("Enter initial cell count: ")
 final_count = input("Enter final cell count: ")
 time_elapsed = input("Enter time elapsed: ")
 
-if initial_count.isdigit() and final_count.isdigit() and time_elapsed.isdigit() and (final_count > initial_count):
+if initial_count.isdigit() and final_count.isdigit() and time_elapsed.isdigit():
     initial_count = int(initial_count)
     final_count = int(final_count)
     time_elapsed = int(time_elapsed)
-    if initial_count > 0 and final_count > 0 and time_elapsed > 0:
+    if initial_count > 0 and final_count > 0 and time_elapsed > 0 and (final_count > initial_count):
         growth_rate = (math.log(final_count)-math.log(initial_count)) / time_elapsed
         print("Growth Rate:", growth_rate)
     else:
