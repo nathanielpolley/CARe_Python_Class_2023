b96218756a198ebed6e4505bb53bf7df0435ec73
maureen <maureen.verdier@univ-tlse3.fr>
2023-10-11 15:51:33 +0200

    changes for exo_0 .py .txt

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,18 @@
-#YOUR CODE FOR EX_0 ADVANCED HERE
+dna_sequence=input('enter the DNA sequence:')
+#check if DNA sequence and length
+dna_sequence=dna_sequence.upper()
+length=len(dna_sequence)
+if(dna_sequence.count("A")+dna_sequence.count("T")+dna_sequence.count("C")+dna_sequence.count("G"))==length:
+    print('valid sequence')
+    print("The sequence length is " + str(length) + " nucleotides long")
+    print("The number of adenosine is:", dna_sequence.count("A"))
+    print("The number of guanine is:", dna_sequence.count("G"))
+    print("The number of thymidine is:", dna_sequence.count("T"))
+    print("The number of cytosine is:", dna_sequence.count("C"))
+else:
+    print('invalid sequence, please enter a DNA sequence')
+
+
+
+
+
