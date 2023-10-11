7724bda518437da783a46e608a0ca0afba179e93
Anna DIAZ <anna.diaz@univ-tlse3.fr>
2023-10-11 19:32:11 +0200

    advanced week0

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,30 +1,19 @@
 #YOUR CODE FOR EX_0 ADVANCED HERE
 
-
-
 dna = (input("Enter a DNA sequence (A, C, G, T only): ")).upper()
 def s_len (dna):
     sequence_length = len(dna)
     return sequence_length
 
-
 #Now we need to count
 
 def count_dna (dna):
-    A_nb = C_nb = G_nb = T_nb = 0
     for base in dna:
-        if base == 'A':
-            A_nb += 1
-        elif base == 'C':
-            C_nb += 1
-        elif base == 'G':
-            G_nb += 1
-        elif base == 'T':
-            T_nb += 1
-        else:
+        if (base != 'A' and base !='C'and base != 'G' and base != 'T'):
             # Error: Invalid base found
             print("Error: this is not a dna sequence !")
-    return [A_nb, C_nb, G_nb, T_nb]
+            return()
+    return [dna.count('A'), dna.count('C'), dna.count('G'), dna.count('T')]
 
 # Calculate GC content
 gc_content = (count_dna(dna)[1]+count_dna (dna)[2]) /  s_len(dna) * 100
@@ -37,6 +26,5 @@ print("G Count:", count_dna(dna)[2])
 print("T Count:", count_dna(dna)[3])
 print("GC Content:", gc_content, '%')
 
-#sequence len and count
-s_len(dna)
-count_dna(dna)
+
+
