35f40e3a77fb14fad3d134aa390572776da5974a
emma10811 <emmanuellaabassey@outlook.com>
2023-10-11 17:14:16 -0500

    completed advanced lvl hw week 0

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -3,3 +3,7 @@ import this
 print("\n"+"Hi! My name is Emmanuella Bassey, and I am from Houston, TX. I enjoy discovering new music, playing volleyball, and completing DIY projects. Through learning python, my goal is to be able to sharpen my bioinformatics skills to potentially work for an industry company with hands in science + medicine.  ")
 
 print("\n"+"The last time I was in a place without a computer available was when I went to a city called 'Lourdes' here in France with some other girls from a summer research program. We were able to take in the beauty and religious history of the place and put flowers in each other's hair upon finding a nice patch. It was a peaceful time!")
+
+
+
+
