2fdaed0a6f3d7bc9f0cd60434515a8e203e0a84e
SilviArk <arcucci.silvia@gmail.com>
2023-10-12 16:05:15 +0200

    I complete the exercise Yuppiii

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,23 @@
-#YOUR CODE FOR EX_0 ADVANCED HERE
+#
+import math
+DNAseq = input("Enter DNA sequence")
+DNAseq = DNAseq.upper()
+
+if any(words not in "ATGC" for words in DNAseq):
+    print("wrong sequence")
+else:
+    lenght = len(DNAseq)
+    A = DNAseq.count('A')
+    C = DNAseq.count('C')
+    G = DNAseq.count('G')
+    T = DNAseq.count('T')
+    GC = G + C
+    GCratio = GC / lenght
+    GCpercent = GCratio * 100
+    print(f"The lenght is: {lenght}")
+    print(f"adenine is: {A}")
+    print(f"cytosine is: {C}")
+    print(f"guanine is: {G}")
+    print(f"thymine is: {T}")
+    print(f"GC content is: {GCpercent}")
+
