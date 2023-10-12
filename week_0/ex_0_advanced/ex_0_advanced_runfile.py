a5c5b6cc42a44b2314338239991e51e6e7a475f4
maureen <maureen.verdier@univ-tlse3.fr>
2023-10-12 10:42:47 +0200

    changes for exo_0 .py .txt

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -9,6 +9,9 @@ if(dna_sequence.count("A")+dna_sequence.count("T")+dna_sequence.count("C")+dna_s
     print("The number of guanine is:", dna_sequence.count("G"))
     print("The number of thymidine is:", dna_sequence.count("T"))
     print("The number of cytosine is:", dna_sequence.count("C"))
+    GC_content=((dna_sequence.count("G")+dna_sequence.count("C"))/length)*100
+    print('The sequence contains ' + str(GC_content) + '% of GC')
+
 else:
     print('invalid sequence, please enter a DNA sequence')
 
