1981efad2d546c821af0757d1ae8ce6fd4648170
Lounaf <loulou.ferraty@orange.fr>
2023-10-14 19:25:33 +0200

    Advanced file _week 0

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -2,3 +2,34 @@
 
 import sys
 print(sys.version)
+
+def Sequence() :
+    DNA_seq = str(input("Enter DNA sequence :"))
+    DNA = (DNA_seq.upper())
+    DNA_letters = "ATGC"
+    count = 0
+    for letter in DNA :
+        if DNA_letters.find(letter) == -1 :
+            count = count + 1
+    if count > 0:
+        print("invalid input, sequence could only contain the following letters (a,t,c,g)")
+    else :
+        print("The sequence lenght is : " + str(len(DNA)))
+        print("The number of A is :" + str(DNA.count("A")))
+        print("The number of T is :" + str(DNA.count("T")))
+        print("The number of G is :" + str(DNA.count("G")))
+        print("The number of C is :" + str(DNA.count("C")))
+        print("The number of GC is :" + str(DNA.count("GC")))
+Sequence()
+
+
+
+
+
+
+
+
+
+
+
+
