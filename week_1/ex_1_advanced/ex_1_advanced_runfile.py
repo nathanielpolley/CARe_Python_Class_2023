4aed3be7e3d1665e73a7aa096e8f3e01aeade3eb
MMT01 <mmt01tricaud@MacBook-Pro-de-MMT01.local>
2023-10-16 23:52:17 +0200

    I hope I'll be able to explain it after and answer to questions, but it works, thanks to saint GPT

--- week_1/ex_1_advanced/ex_1_advanced_runfile.py
+++ week_1/ex_1_advanced/ex_1_advanced_runfile.py
@@ -1,1 +1,58 @@
-#YOUR CODE FOR EX_1 ADVANCED HERE
+# Import necessary libraries
+import requests  # Library for making HTTP requests
+import re  # Library for regular expressions (pattern searching and manipulation)
+from fuzzywuzzy import fuzz  # Library for string similarity comparison
+
+# Define a function to fetch references from a given URL
+def fetch_references_from_url(url):
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+    # Split the response text into lines and return as a list
+    return response.text.splitlines()
+
+# Define a function to normalize a given reference
+def normalize_reference(ref):
+
+    # Remove all non-alphabetic characters and convert to lowercase
+    ref = re.sub(r'[^a-zA-Z\s]', '', ref).lower()
+    # Remove extra white spaces and return
+    return ' '.join(ref.split())
+
+# Define a function to compare references from two URLs
+def compare_references():
+    """
+    Compares references from two predefined URLs and writes the comparison result to an output file.
+    The references are considered similar if their normalized forms have a similarity ratio > 85.
+    """
+
+    # Define the URLs containing references
+    url1 = "https://raw.githubusercontent.com/mt93git/CARe_Python_Class_20231/d014e499f23a9939af4b08dd4fff32445a0248c3/week_1/ex_1_advanced/references_1.txt"
+    url2 = "https://raw.githubusercontent.com/mt93git/CARe_Python_Class_20231/d014e499f23a9939af4b08dd4fff32445a0248c3/week_1/ex_1_advanced/references_2.txt"
+
+    # Fetch the list of references from the URLs
+    references_1 = fetch_references_from_url(url1)
+    references_2 = fetch_references_from_url(url2)
+
+    # Open an output file to write the comparison results
+    with open("output.txt", "w") as out_file:
+        # Loop through each pair of references from the two files
+        for i, (ref1, ref2) in enumerate(zip(references_1, references_2), start=1):
+            # Normalize the references
+            normalized_ref1 = normalize_reference(ref1)
+            normalized_ref2 = normalize_reference(ref2)
+
+            # Compare the similarity of the normalized references using a threshold of 85
+            if fuzz.partial_ratio(normalized_ref1, normalized_ref2) > 85:
+                # Write the "GOOD" status to the output file if references are similar
+                out_file.write(f"{i} : GOOD\n")
+            else:
+                # Write the "DIVERGENT" status and the original references to the output file if they are divergent
+                out_file.write(f"{i} : DIVERGENT\n")
+                out_file.write(f"  In file 1: {ref1}\n")
+                out_file.write(f"  In file 2: {ref2}\n")
+
+# Entry point for the script
+if __name__ == "__main__":
+    # Call the function to compare references
+    compare_references()
--- week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
+++ week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
@@ -1,1 +1,32 @@
-#YOUR CODE FOR EX_1 INTERMEDIATE HERE
+# Initialize dictionaries to store references from the two files
+# Key: Line number, Value: Reference text without whitespaces
+ref1_dict = {}  # Creates an empty dictionary to hold references from the first file
+ref2_dict = {}  # Creates an empty dictionary to hold references from the second file
+
+# Initialize a dictionary to keep track of the comparison status
+# Key: Line number, Value: List containing either 'GOOD' or differences
+status = {}  # Creates an empty dictionary to store the results of the comparison
+for i in range(1, 61):  # Goes through numbers 1-60 to populate status dictionary keys
+    status[str(i)] = []  # Sets the value of each key to an empty list to store status info
+
+# Open both files and read them line by line
+with open("references_1.txt", "r") as f1, open("references_2.txt", "r") as f2:
+    for i, (line1, line2) in enumerate(zip(f1, f2), 1):  # Reads both files line by line
+        # Remove any trailing and internal whitespaces
+        ref1_dict[str(i)] = line1.strip().replace(" ", "")  # Populates the first dictionary with cleaned-up text from the first file
+        ref2_dict[str(i)] = line2.strip().replace(" ", "")  # Populates the second dictionary with cleaned-up text from the second file
+
+# Loop through the dictionaries to compare each line
+for key in ref1_dict:  # Goes through each key (which is the line number) in the first dictionary
+    if ref1_dict[key] == ref2_dict[key]:  # Compares the values (text content) of both dictionaries for each line
+        status[key].append("GOOD")  # Appends "GOOD" to the list value of the corresponding key in the 'status' dictionary
+    else:
+        # Here, we're comparing each character of the strings to find differences
+        differences = [char1 for char1, char2 in zip(ref1_dict[key], ref2_dict[key]) if char1 != char2]  # Finds and stores differences in a list
+        status[key].extend(differences)  # Adds those differences to the 'status' dictionary for that line number
+
+# Write the results to a new text file
+with open("ex_1_advanced_submit.txt", "w") as output:  # Opens a new file to write the results
+    for key, value in status.items():  # Goes through each item in the 'status' dictionary
+        output.write(f"{key} : {', '.join(value)}\n")  # Writes the status result for each line in the output file
+
