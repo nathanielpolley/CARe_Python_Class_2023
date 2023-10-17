021d6281aebbab8f2db514a9922c88007359580d
emma10811 <emmanuellaabassey@outlook.com>
2023-10-17 09:12:23 -0500

    redone with suggestions again! adv hw week 0

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,12 +1,16 @@
 #YOUR CODE FOR EX_0 ADVANCED HERE
 
-DNA_Seq = input("Enter your DNA Sequence Here: ")
+DNA_Seq = input.upper("ENTER YOUR DNA SEQUENCE HERE: ") or input.lower("enter your dna sequence here")
+if i in DNA_Seq != 'A' or 'T' or 'C' or 'G' or 'a' or 't' or 'c' or'g':
+    print("Please enter a valid sequence. Try again!") #to ensure upper + lowercase is acceptable
+
 
 
 #Alternative to using multiple for lopps!!!! so great thanks!
 from collections import Counter
 counter = Counter(DNA_Seq)
 counter['A'], counter['T'], counter['C'], counter['G'] #gives me the exact # of specific bases!
+
 
 #OR
 adenine = counter['A']
@@ -15,7 +19,6 @@ guanine = counter['G']
 cytosine = counter['C']
 
 print(adenine, thymine, guanine, cytosine)
-
 #find GC content
 
 
