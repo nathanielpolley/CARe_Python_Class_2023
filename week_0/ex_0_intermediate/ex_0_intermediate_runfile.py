d6c7d8d53f1379b72e7ebedeb593d49f90b23be9
emma10811 <emmanuellaabassey@outlook.com>
2023-10-17 09:18:59 -0500

    reordered according to suggestions! int hw week 0

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -7,14 +7,15 @@
 #- Calculate and display the growth rate using the formula: Growth Rate = (ln(final count) - ln(initial count)) / time.
 #- Ensure that the program handles invalid input gracefully.
 
-
+#order of code changed!
 import math
-initial = int(input("Enter Initial Bacteria Count: "))
-final = int(input("Enter Final Bacteria Count: "))
-time = int(input("Enter Amount of Time Growth Has Occurred For: "))
 def MBgrowthrate(initial,final,time):
-    if final > initial and initial.isdigit() and final.isdigit() and time.isdigit():
-        growth = (math.log(final)) - (math.log(initial)/time)
+    if initial.isdigit() and final.isdigit() and time.isdigit():
+        initial = int(input("Enter Initial Bacteria Count: "))
+        final = int(input("Enter Final Bacteria Count: "))
+        time = int(input("Enter Amount of Time Growth Has Occurred For: "))
+        if final > initial:
+            growth = (math.log(final)) - (math.log(initial)/time)
         print(growth)
     else:
         print("Enter a valid input please! :-)")
