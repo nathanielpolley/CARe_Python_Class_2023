e295a68852994f333c9f74f078c6bfe2c1f3a3ef
Hélène <helene.cab@outlook.fr>
2023-10-19 12:36:03 +0200

    2 modifications

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,16 @@
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+
+initial_count = input("Enter the initial cell count:")
+final_count = input("Enter the final cell count:")
+time = input("Enter the time elapsed ( in hours):")
+
+initial_count = float(initial_count)
+final_count = float(final_count)
+time = float(time)
+
+growth_rate = (math.log(final_count) - math.log(initial_count)) / time
+
+if growth_rate is not None :
+    print(f"The growth rate of the microbial culture is : {growth_rate} per hours")
+else :
+    print("Invalid input")
