b2b0e398929ddef7e2d41bc69589b692c85927d3
maureen <maureen.verdier@univ-tlse3.fr>
2023-11-02 17:06:19 +0100

    changes for exo_0 .py .txt

--- week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
+++ week_1/ex_1_intermediate/ex_1_intermediate_runfile.py
@@ -11,25 +11,25 @@ print(str(count)+' samples have been collected')
 
 #list comprehension to create a list of microbial species with sample counts greater than 15
 greater_than_15=[species for species, count in microbial_species.items() if count >15]
-print(greater_than_15)
+print('We have collected more than 15 '+ str(greater_than_15))
 
 #Create a function
 def add_species ():
-    nom_champi=input("enter the new species name: ")
-    if microbial_species.get(nom_champi):
-        nombre_champi=microbial_species.get(nom_champi)+1
-        return microbial_species.update({nom_champi:nombre_champi})
+    new_species
+    if microbial_species.get(new_species):
+        nombre_champi=microbial_species.get(new_species)+1
+        return microbial_species.update({new_species:nombre_champi})
     else:
-        return microbial_species.update({nom_champi:1})
+        return microbial_species.update({new_species:1})
 
 #While loop to continuously prompt the user for new species names
 
-new_s=input('Enter a new species name: ')
-while len(new_s)>0:
+new_species=input('Enter a new species name: ')
+while len(new_species)>0:
     add_species()
-    new_s=input('Enter a new species name: ')
+    new_species=input('Enter a new species name: ')
 
-print(microbial_species)
+print('The new dictonnary is now '+str(microbial_species))
 
 
 
