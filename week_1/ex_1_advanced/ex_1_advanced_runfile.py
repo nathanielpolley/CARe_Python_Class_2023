67acf593eac282b4e1b5e3ca7ba0f7d5d2b9bc55
maureen <maureen.verdier@univ-tlse3.fr>
2023-11-06 15:32:00 +0100

    changes for exo_0 .py .txt

--- week_1/ex_1_advanced/ex_1_advanced_runfile.py
+++ week_1/ex_1_advanced/ex_1_advanced_runfile.py
@@ -1,1 +1,47 @@
-#YOUR CODE FOR EX_1 ADVANCED HERE
+#references1
+# Open a file in read mode
+file_path_1 = 'references_1.txt'
+with open(file_path_1, 'r', encoding='utf8') as file:
+    lines = file.readlines()
+#cleaning up the list
+for i in range (len(lines)):
+    lines[i]=lines[i].replace('\n','') #remove line breaks
+    lines[i]=lines[i][3:] #remove the number before the reference
+    lines[i]=lines[i].replace(' ','') #remove whitespaces
+# Iterate through the list using enumerate to create a dictionary
+my_dict = {str(index+1): value for index, value in enumerate(lines)}
+print('Here is the dictionnary for reference_1.txt'+str(my_dict))
+
+#references_2
+file_path_2 = 'references_2.txt'
+with open(file_path_2, 'r', encoding='utf8') as file:
+    lines2 = file.readlines()
+#cleaning up the list
+for i in range (len(lines2)):
+    lines2[i]=lines2[i].replace('\n','') #remove line breaks
+    lines2[i]=lines2[i][3:] #remove the number before the reference
+    lines2[i]=lines2[i].replace(' ','') #remove whitespaces
+# Iterate through the list using enumerate to create a dictionary
+my_dict2 = {str(index+1): value for index, value in enumerate(lines2)}
+print('Here is the dictionnary for reference_2.txt'+str(my_dict2))
+
+#status dictionnary
+status={key:'' for key in range(1,61)}
+
+#comparison of the dictionnaries
+for i in range (60):
+    if lines[i]==lines2[i]:
+        status[i]='good'
+    else:
+        status[i]='mismatch detected'
+print(status)
+
+
+
+
+
+
+
+
+
+
